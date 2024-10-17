a628fce155283a11cbdbc7dc0715c94f1f6d64c8
mdnumanahmed <mnumansyl@gmail.com>
2024-10-17 21:39:41 +0600

    created getter and setter function and updated total price in UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 // Select Elements from HTML DOM
-const seatsLeft = document.getElementById("seats-left");
-const selectedSeatCount = document.getElementById("selected-seat-count");
-const totalPrice = document.getElementById("total-price");
-const grandTotal = document.getElementById("grand-total");
-const couponInput = document.getElementById("coupon-input");
-const tableBody = document.getElementById("table-body");
+// const seatsLeft = document.getElementById("seats-left");
+// const selectedSeatCount = document.getElementById("selected-seat-count");
+// const totalPrice = document.getElementById("total-price");
+// const grandTotal = document.getElementById("grand-total");
+// const couponInput = document.getElementById("coupon-input");
+// const tableBody = document.getElementById("table-body");
 
 const allSeats = document.getElementsByClassName("seat");
 for (const seat of allSeats) {
@@ -18,7 +18,19 @@ for (const seat of allSeats) {
         : seatName.charAt(0) === "C"
         ? "Premium Economy"
         : "Economy";
-    // console.log(event.target.innerText);
-    console.log(className);
+    let totalPrice = getInnerTextValue("total-price");
+    totalPrice = totalPrice + 500;
+    setInnerTextValue("total-price", totalPrice);
   });
+}
+
+function getInnerTextValue(id) {
+  const innerTextValue = document.getElementById(id).innerText;
+  const convertedValue = parseInt(innerTextValue);
+  return convertedValue;
+}
+
+function setInnerTextValue(id, value) {
+  const innerTextValue = (document.getElementById(id).innerText = value);
+  return innerTextValue;
 }
