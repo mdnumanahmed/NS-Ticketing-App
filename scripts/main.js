8611212b31f33019158f5a58c6d88c31504c2283
mdnumanahmed <mnumansyl@gmail.com>
2024-10-17 23:08:14 +0600

    Showed Selected Seat info

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,11 @@
 // const tableBody = document.getElementById("table-body");
 
 const allSeats = document.getElementsByClassName("seat");
+// let seatsLeft = 40;
+// let seatCount = 0;
 for (const seat of allSeats) {
   seat.addEventListener("click", function (event) {
+    const seatElement = event.target;
     const seatName = event.target.innerText;
     const className =
       seatName.charAt(0) === "A"
@@ -18,19 +21,38 @@ for (const seat of allSeats) {
         : seatName.charAt(0) === "C"
         ? "Premium Economy"
         : "Economy";
+
+    //   Display selected Data
+    showSelecetSeatInfo(seatName, className, 500);
+    //   Total Price update
     let totalPrice = getInnerTextValue("total-price");
     totalPrice = totalPrice + 500;
     setInnerTextValue("total-price", totalPrice);
+    setInnerTextValue("grand-total", totalPrice);
   });
 }
 
+// Get converted value generic function
 function getInnerTextValue(id) {
   const innerTextValue = document.getElementById(id).innerText;
   const convertedValue = parseInt(innerTextValue);
   return convertedValue;
 }
 
+// Set value generic function
 function setInnerTextValue(id, value) {
   const innerTextValue = (document.getElementById(id).innerText = value);
   return innerTextValue;
+}
+
+// Display seat booking info and cost function
+function showSelecetSeatInfo(seatName, className, price) {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML += `
+    <tr>
+        <td>${seatName}</td>
+        <td>${className}</td>
+        <td>${price}</td>
+    </tr>
+    `;
 }
